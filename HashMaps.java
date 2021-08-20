539b809520df8ceaeafa55da2fb517447aeaa39c
yuntaekOhO <oyt1996@naver.com>
2021-08-20 21:47:35 +0900

    modified comment in .java

--- HashMaps.java
+++ HashMaps.java
@@ -67,8 +67,7 @@ public class HashMaps {
 		hm3.put("윤", 5);
 		Set<Entry<String, Integer>> set = hm3.entrySet();
 		//Set set = hm3.entrySet() 와 같다
-		System.out.print("Set entrySet() : ");
-		System.out.println(set);
+		System.out.println("Set entrySet() : " + set);
 		//HashMap에 저장된 key,value 엔트리 형태로 set에 저장
 		
 		System.out.println("getOrDefault() - 찾는 키 있음 : "+hm3.getOrDefault("이", 0) +
@@ -76,26 +75,26 @@ public class HashMaps {
 		// 찾는 key가 존재한다면 찾는 key의 value 리턴, 없다면 default 리턴
 		// getOrDefault(search key, default value)
 		
-		System.out.print(hm3.putIfAbsent("윤", 6));
-		System.out.println(hm3.putIfAbsent("송", 6));
-		System.out.println("putIfAbsent(key, value) : "+hm3);
+		hm3.putIfAbsent("송", 6);
+		System.out.println("Map에 있는 key= 윤, value= "+hm3.putIfAbsent("윤", 6));
+		System.out.println("putIfAbsent(송, 6) 추가 됨 : "+hm3);
 		//key값 있으면 해당 key의 value 리턴 ,
 		//key값 없으면 key와 value 추가
 		
 		hm3.put("A", 7);
-		System.out.println(hm3.compute("A", (s, integer) -> integer+1));
+		System.out.println("해당 key의 값 변경 key= A value= "+hm3.compute("A", (s, integer) -> integer+1));
 		System.out.println("compute(key, Function)"+hm3);
-		//key의 값에 대해서 어떻게 연살할지 정의한다. 기존의 키에 대한 값이 없는 경우 V는 null로 생성된다.
+		//key의 값에 대해서 어떻게 연산할지 정의한다. 기존의 키에 대한 값이 없는 경우 V는 null로 생성된다.
 		
 		hm3.computeIfAbsent("A", key -> 10); //key가 A인것은 있음  
 		hm3.computeIfAbsent("B", key -> 10); //key가 B인것은 없음 = key "B" 만들고 value 10 
-		System.out.println(hm3.get("A"));	// =value 8
-		System.out.println(hm3.get("B"));	// =value 10
+		System.out.println("computeIfAbsent() 후 get(A) : "+hm3.get("A"));	// =value 8
+		System.out.println("computeIfAbsent() 후 get(B) : "+hm3.get("B"));	// =value 10
 		System.out.println("computeIfAbsent(key, Function) 실행 후 : "+hm3);
-		//compute와 비슷하다. key 값이 없을 경우에 실행된다.
+		//compute와 비슷하다. key 값이 없을 경우에 function 실행.
 		
-		System.out.println(hm3.computeIfPresent("B", (s, number) -> number*number));
-		System.out.println(hm3.computeIfPresent("C", (s, number) -> 20));
+		System.out.println("key= B 가 있으면 function 수행"+hm3.computeIfPresent("B", (s, number) -> number*number));
+		System.out.println("key= C 없어서 function 미수행 "+hm3.computeIfPresent("C", (s, number) -> 20));
 		System.out.println("computeIfPresent(key, function) : "+hm3);
 		//key "B"가 있으면 Function 실행
 		
